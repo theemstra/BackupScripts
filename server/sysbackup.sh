946ea259a3258eea698b6e9208c09fd3f9fa5a06
Thom <thom@heemstra.us>
2015-01-05 21:06:09 +0100

    Added licensing information to files

--- server/sysbackup.sh
+++ server/sysbackup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-# XSByte 2015
+# Backup & Rotation Scripts
+# Copyright 2010-2015 by XSByte
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# @category    XSByte
+# @copyright   2010-2015 by XSByte | http://www.xsbyte.com
+# @author      Thom Heemstra <thom@heemstra.us>
+# @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
 
 #
 ##
